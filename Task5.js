2ea390de41ec51cddf8d79afacf56e30c22d65b6
aswinbabu01 <151752328+aswinbabu01@users.noreply.github.com>
2023-12-11 01:38:08 +0530

    Add files via upload

---
+++ Task5.js
@@ -0,0 +1,74 @@
+//For In//
+
+// let Obj = {
+//     name: "aswin",
+//     age: 23,
+//     Qualification: "B.sc",
+//     Gender: "male"
+// }
+// for (let x in Obj) {
+//     console.log(x + ":" + Obj[x]);
+// }
+
+//For//
+
+// let Arr = [1, 2, 3, 4, 5, 6];
+// for (let i = 0; i < Arr.length; i++) {
+//     console.log(Arr[i])
+// }
+
+//For Of//
+
+// let Arr1 = [1, 2, 3, 4, 5, 6];
+// for (let x of Arr1) {
+//     console.log(x);
+// }
+
+//ForEach//
+
+// let Arr2 = [3, 5, 7, 9, 6, 4];
+// Arr2.forEach(x => {
+//     console.log(x)
+// });
+
+//______________________________________________________//
+
+// {
+//     "Resume" = {
+//         "name": "Aswin",
+//         "adress": "236 gandhi Street, ashoknagar, state 625612",
+//         "Phone": "8812365546",
+//         "email": "aswinmj001@gmail.com",
+//     },
+//         "Summary": "Highly organized and self - motivated unique storyteller seeks the position of Xyz(name of the field) journalist.Bringing excellent verbal and written communication skills with two years of experience working in a multimedia environment.Ability to deliver promotable and compelling stories to the companys website and social media platforms."
+//     "education": [
+//         {
+//             "degree": "Bachelor of Science in animation and Vfx",
+//             "School": "kendraVidyalayam.Hr.sec school"
+//             "graduation Year": "2021"
+
+//         }
+//     ],
+//         "experience" : [
+//             {
+//                 "position": "3D Surfacing artist",
+//                 "company": "Technicolor",
+//                 "location": "Bengaluru,Karnataka",
+//                 "start_date": "july 2022",
+//                 "end_date": "April 2023",
+//             }
+//         ],
+//             "Skills": [
+//                 "softwares-Fluency": "Autodesk Maya, Foundry Mari, Adobe Photoshop, Adobe Substance Painter, Adobe Premire Pro, Blender, Zbrush",
+
+//                 "Problem-Solving and Analytical Skills",
+
+//                 "Team collaboration and Communication"
+//             ],
+//             {
+//                 "languages": "English"
+//             "proficiency": "intermediate"
+//             }
+// }
+
+//______________________________________________//
